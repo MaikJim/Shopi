05d332eca0d67979b0593d7e2db5564e5287db92
MaikJim <maikoljimenez427@gmail.com>
2023-10-19 23:06:07 -0400

    Inicializacion del proyecto, configuracion del entorno de desarrollo y creacion de los primeros componentes

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Card from "../../Components/Card/Card";
+function Home() {
+  return (
+    <div>
+      Home
+      <Card />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Pages/App/App";
+import "./index.css";
+import Layout from "./Components/Layout/Layout";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Layout>
+      <App />
+    </Layout>
+  </React.StrictMode>
+);
